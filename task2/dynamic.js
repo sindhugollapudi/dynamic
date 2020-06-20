4f6fd33f877b2872de120e84e6ccab3bf65468f8
sindhu <gssvstejaswi@gmail.com>
2020-06-20 12:18:10 +0530

    dynamic

---
+++ task2/dynamic.js
@@ -0,0 +1,134 @@
+var root=document.getElementById("main");
+var div1=document.createElement('div');
+div1.classList.add("sub");
+root.appendChild(div1);
+
+
+
+var image=document.createElement('img');
+image.src="profile.png";
+image.alt="profile picture";
+image.classList.add('img');
+div1.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="Sindhu";
+div1.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="Student"
+div1.classList.add("p");
+div1.appendChild(para);
+
+
+
+
+var para1=document.createElement('p');
+para1.textContent="gssvstejaswi@gmail.com"
+div1.classList.add("p");
+div1.appendChild(para1);
+
+
+var para2=document.createElement('p');
+para2.textContent="9603384612"
+div1.classList.add("p");
+div1.appendChild(para2);
+
+
+
+
+var anc=document.createElement('a');
+anc.href="resume.html"
+anc.textContent="viewprofile";
+div1.classList.add('a');
+div1.appendChild(anc);
+
+
+//===========
+var div2=document.createElement('div');
+div2.classList.add("sub");
+root.appendChild(div2);
+
+
+
+var image=document.createElement('img');
+image.src="profile.png";
+image.alt="profile picture";
+image.classList.add('img');
+div2.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="Sindhu";
+div2.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="Student"
+para.classList.add("p");
+div2.appendChild(para);
+
+
+
+
+var para1=document.createElement('p');
+para1.textContent="gssvstejaswi@gmail.com"
+div2.classList.add("p");
+div2.appendChild(para1);
+
+
+var para2=document.createElement('p');
+para2.textContent="9603384612"
+div2.classList.add("p");
+div2.appendChild(para2);
+
+
+
+
+var anc=document.createElement('a');
+anc.href="resume.html"
+anc.textContent="viewprofile";
+anc.classList.add('a');
+div2.appendChild(anc);
+//====================
+var div3=document.createElement('div');
+div3.classList.add("sub");
+root.appendChild(div3);
+
+
+
+var image=document.createElement('img');
+image.src="profile.png";
+image.alt="profile picture";
+image.classList.add('img');
+div3.appendChild(image);
+
+var heading=document.createElement('h2');
+heading.textContent="Sindhu";
+div3.appendChild(heading);
+
+var para=document.createElement('p');
+para.textContent="Student"
+para.classList.add("p");
+div3.appendChild(para);
+
+
+
+
+var para1=document.createElement('p');
+para1.textContent="gssvstejaswi@gmail.com"
+div3.classList.add("p");
+div3.appendChild(para1);
+
+
+var para2=document.createElement('p');
+para2.textContent="9603384612"
+div3.classList.add("p");
+div3.appendChild(para2);
+
+
+
+
+var anc=document.createElement('a');
+anc.href="resume.html"
+anc.textContent="viewprofile";
+anc.classList.add('a');
+div3.appendChild(anc);
